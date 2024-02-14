a05afa7c969760af26d24284fc7cc465ea09e680
Aymeric Pellé <aympelle@gmail.com>
2024-02-14 13:14:40 +0100

    v0.4.0:#45: Improve Temgen API: Add treat_temgen_template() and use **kwargs.
    treat_temgen_file()
    treat_temgen_template()
    They can take execution_context or (ui and variables).

--- temgen.py
+++ temgen.py
@@ -2,7 +2,6 @@ import xml.etree.ElementTree as XMLTree
 from pathlib import Path
 
 import temgen_node
-from ask_dialog import AskDialog
 from execution_context import ExecutionContext
 from template_tree_info import TemplateTreeInfo
 from variables_dict import VariablesDict
@@ -19,12 +18,34 @@ class Temgen:
             return temgen_node.TemgenNode.treat_temgen_node(tree.getroot(), execution_context, tree_info)
 
     @staticmethod
-    def treat_temgen_file(temgen_filepath: Path, ui: AskDialog,
-                          init_variables: None | VariablesDict = None,
-                          output_dir: Path = Path.cwd()):
-        if init_variables is None:
-            init_variables = VariablesDict()
-        execution_context = ExecutionContext(ui, init_variables)
+    def treat_temgen_file(temgen_filepath: Path, **kwargs):
+        execution_context = kwargs.get("execution_context", None)
+        if execution_context is None:
+            ui = kwargs["ui"]
+            init_variables = kwargs.get("variables", VariablesDict())
+            execution_context = ExecutionContext(ui, init_variables)
+        else:
+            assert "ui" not in kwargs
+            assert "variables" not in kwargs
+        output_dir = kwargs.get("output_dir", Path.cwd())
         tree_info = TemplateTreeInfo(current_temgen_filepath=temgen_filepath,
+                                     variables=execution_context.init_variables,
+                                     current_dirpath=output_dir)
+        Temgen.treat_tree_current_temgen_file(execution_context, tree_info)
+
+    @staticmethod
+    def treat_temgen_template(temgen_path: Path, version: str | None = None, **kwargs):
+        execution_context = kwargs.get("execution_context", None)
+        if execution_context is None:
+            ui = kwargs["ui"]
+            init_variables = kwargs.get("variables", VariablesDict())
+            execution_context = ExecutionContext(ui, init_variables)
+        else:
+            assert "ui" not in kwargs
+            assert "variables" not in kwargs
+        temgen_filepath = execution_context.find_temgen_file(temgen_path, version)
+        output_dir = kwargs.get("output_dir", Path.cwd())
+        tree_info = TemplateTreeInfo(current_temgen_filepath=temgen_filepath,
+                                     variables=execution_context.init_variables,
                                      current_dirpath=output_dir)
         Temgen.treat_tree_current_temgen_file(execution_context, tree_info)
