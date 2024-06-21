45db4ffcab78a557baddcfe46e8e02e5e49c5dc4
Aymeric Pellé <aympelle@gmail.com>
2024-06-21 15:15:49 +0200

    v0.7.0:#77: The unit test var_file__valid_var_file uses --var-file with several files.

--- tests/test_temgen_program.py
+++ tests/test_temgen_program.py
@@ -75,10 +75,11 @@ class TestTemgenProgram(TestTemgenProgramBase):
 
     def test__var_file__valid_var_file__ok(self):
         output_root_dir = "var_file__valid_var_file"
-        args = ['--var-file', 'input/var_files/texts.json']
+        args = ['--var-file', 'input/var_files/texts.json', 'input/var_files/fruits.json']
         var_defs = '<var name="text" />\n<var name="other_text" />'
         self._test_generated_trivial_template_file(output_root_dir, argv=args,
-                                                   var_definitions=var_defs, file_contents=":{text}:{other_text}:")
+                                                   var_definitions=var_defs,
+                                                   file_contents=":{text}:{other_text}:{a_fruit}:")
 
     def test__var_file__unknown_var_file__exception(self):
         json_fpath = 'input/var_files/not_found.json'
