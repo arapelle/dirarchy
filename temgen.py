a4fd0af2e6e04a2b71feab588809b49382c6b0a9
Aymeric Pellé <aympelle@gmail.com>
2024-07-04 11:21:57 +0200

    v0.8.0: Start dev version.

--- temgen.py
+++ temgen.py
@@ -22,7 +22,7 @@ from variables.variables_dict import VariablesDict
 
 
 class Temgen:
-    VERSION = semver.Version.parse('0.7.0')
+    VERSION = semver.Version.parse('0.8.0-dev')
     APPLICATION_DIRECTORIES = ApplicationDirectories(names.LOWER_PROGRAM_NAME)
 
     def __init__(self, basic_ui: AbstractBasicUi | None, logger=None, **kargs):
