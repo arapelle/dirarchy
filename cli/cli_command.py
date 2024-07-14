e49832a347519997c5e7552b7897e4ee69873309
Aymeric Pellé <aympelle@gmail.com>
2024-07-14 09:38:29 +0200

    WIP

---
+++ cli/cli_command.py
@@ -0,0 +1,49 @@
+import re
+from argparse import ArgumentParser
+
+
+class CliCommand:
+    def __init__(self, subparsers=None, arg_name=None, default=None):
+        self.__subcommand_label = arg_name if arg_name else self.default_subcommand_label()
+        if subparsers is None:
+            self.__arg_parser: ArgumentParser = ArgumentParser(self.command_label(),
+                                                               argument_default={"temgen-subcommand": default})
+        else:
+            self.__arg_parser: ArgumentParser = subparsers.add_parser(self.command_label(), help=self.command_label(),
+                                                                      argument_default={
+                                                                          "temgen-subcommand": default})
+
+    def arg_parser(self):
+        return self.__arg_parser
+
+    def subcommand_label(self):
+        return self.__subcommand_label
+
+    def default_subcommand_label(self):
+        return f"{self.command_label()}-subcommand"
+
+    def command_label(self):
+        name_comps = re.findall('[A-Z][^A-Z]*', self.__class__.__name__)
+        return '-'.join([x.lower() for x in name_comps])
+
+    def invoke(self, args=None):
+        if args is None:
+            args = self.__arg_parser.parse_args()
+        print(f"subcommand: {self.subcommand_label()}")
+        arg_name = getattr(args, self.subcommand_label())
+        print(f"  arg_name:  {arg_name}")
+        arg_name = arg_name.replace('-', '_')
+        print(f"  arg_name_: {arg_name}")
+        if hasattr(self.__class__, arg_name):
+            method = getattr(self.__class__, arg_name)
+            delattr(args, self.subcommand_label())
+            print(f"  method found -> call self.{arg_name}(args)")
+            method(self, args)
+        elif hasattr(self, arg_name):
+            field = getattr(self, arg_name)
+            delattr(args, self.subcommand_label())
+            print(f"  attr found -> call {arg_name}.invoke(args)")
+            field.invoke(args)
+        else:
+            print(f"error: Missing field or method '{arg_name}' in class {self.__class__.__name__}.")
+            exit(-1)
